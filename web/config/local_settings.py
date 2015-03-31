0e74e247d5dd8cb82c982cc757ecd81fff2c5e3d
Nic Grayson <nic.grayson@banno.com>
2015-03-31 15:29:07 -0500

    allow whisper dir to be set

--- web/config/local_settings.py
+++ web/config/local_settings.py
@@ -11,4 +11,7 @@ if os.getenv("CLUSTER_SERVERS"):
 if os.getenv("MEMCACHE_HOSTS"):
     CLUSTER_SERVERS = os.getenv("MEMCACHE_HOSTS").split(',')
 
+if os.getenv("WHISPER_DIR"):
+    WHISPER_DIR = os.getenv("WHISPER_DIR")
+
 SECRET_KEY = str(datetime.now())
